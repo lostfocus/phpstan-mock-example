f70917e0f14067668083d0a7ef53ae9b26ae3753
Dominik Schwind <dschwind@lostfocus.de>
2023-05-10 07:10:45 +0200

    Union Types

--- tests/FirstTestClassTest.php
+++ tests/FirstTestClassTest.php
@@ -8,7 +8,7 @@ use PHPUnit\Framework\TestCase;
 
 class FirstTestClassTest extends TestCase
 {
-    private SecondTestClassInterface|MockObject $secondTestClass;
+    private SecondTestClassInterface&MockObject $secondTestClass;
 
     public function setUp(): void
     {
